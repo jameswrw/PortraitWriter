2f03c01481589920475412fb34c3ddb463af7c03
James Weatherley <james.weatherley@citrix.com>
2018-02-27 00:00:55 +0000

    Initial Commit

---
+++ PortraitWriter/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  PortraitWriter
+//
+//  Created by JamesW on 27/02/2018.
+//  Copyright © 2018 JamesW. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
